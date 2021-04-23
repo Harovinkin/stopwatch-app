b6199ecb2835014c9452664a42f51ce31bb999e3
Harovinkin <kharovinkin@gmail.com>
2021-04-23 08:10:03 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { interval, Subject } from "rxjs";
+import { takeUntil, map, filter, buffer, debounceTime, repeatWhen, tap } from "rxjs/operators";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const START = 'start';
+const STOP ='stop';
+
+
+const click$ = new Subject();
+
+const doubleClick$ = click$.pipe(
+  buffer(
+    click$.pipe(debounceTime(300))
+  ),
+  map(list => {
+    return list.length;
+  }),
+  filter(x => x === 2),
+)
+
+const action$ = new Subject();
+
+const stop$ = action$.pipe(filter(action => action === STOP));
+const start$ = action$.pipe(filter(action => action === START));
+
+const countUp$ = interval(1000).pipe(
+takeUntil(stop$),
+takeUntil(doubleClick$.pipe(
+  tap(console.log)
+)),
+repeatWhen(() => start$),
+)
+
+
+export default function App() {
+const [counter, setCounter] = useState(0);
+const [startActive, setStartActive] = useState(true);
+
+useEffect(() => {
+  const sub = countUp$.subscribe(val => setCounter(prev => prev + 1000));
+  action$.next(STOP);
+
+  return () => sub.unsubscribe()
+}, [])
+
+const handleStart = () => {
+  action$.next(START);
+  setStartActive(prev => !prev)
 }
 
-export default App;
+
+const handleStop = () => {
+  action$.next(STOP);
+  setCounter(0)
+  setStartActive(prev => !prev)
+}
+
+const handleWait = () => {
+  doubleClick$.next()
+  setStartActive(true)
+}
+
+const handleReset = () => {
+  setCounter(0)
+}
+
+
+return (
+  <div className='container'>
+    <h1>{new Date(counter).toISOString().slice(11, 19)}</h1>
+    <div className='btn-wrapper'>
+    {startActive
+      ? (<button className='start' onClick={handleStart}>Start</button>)
+      : (<button className='stopt' onClick={handleStop}>Stop</button>)
+    }
+    <button className='wait' onClick={handleWait}>Wait</button>
+    <button className='reset' onClick={handleReset}>Reset</button>
+    </div>
+  </div>
+)
+}
